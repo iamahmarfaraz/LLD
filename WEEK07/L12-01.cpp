330bf2904d17063c13e3ef410b6df44399b7fcda
Ahmar Faraz <138380197+iamahmarfaraz@users.noreply.github.com>
2025-08-13 21:59:16 +0530

    Added Mediator Pattern

---
+++ WEEK07/L12-01.cpp
@@ -0,0 +1,17 @@
+// MEDIATOR DESIGN PATTERN
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <list>
+#include <set>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
